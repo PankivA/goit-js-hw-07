719aba36cd2464cf6ce46fb70bc1b4a9065ce8eb
Andrii Pankiv <pankivand@gmail.com>
2023-03-04 13:55:53 +0200

    add 2 done exepcises

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,44 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
-console.log(galleryItems);
+const gallery = document.querySelector('.gallery');
+const galleryMarkup = galleryItems.map(({ preview, original, description }) => {
+
+   return `<div class="gallery__item">
+        <a class="gallery__link" href="${original}">
+            <img
+                class="gallery__image"
+                src="${preview}"
+                data-source="${original}"
+                alt="${description}"
+            />
+        </a>
+    </div>`;
+}).join('');
+
+gallery.insertAdjacentHTML('beforeend', galleryMarkup);
+gallery.addEventListener('click', onClickOpenModal);
+
+
+function onClickOpenModal(event) {
+    event.preventDefault();
+
+    if (event.target.nodeName !== "IMG") {
+        return;
+    }
+
+    openOrCloseModal(event);
+} 
+
+function openOrCloseModal(e) {
+    const galleryItemModal = basicLightbox.create(`<img src="${e.target.dataset.source}" width="800" height="600">`);
+    
+    galleryItemModal.show();
+
+    document.addEventListener("keydown", e => {
+        if (e.key === "Escape" || e.code === "Escape") {
+            galleryItemModal.close();
+        }
+    
+    });
+} 
--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,28 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
-console.log(galleryItems);
+
+const gallery = document.querySelector('.gallery');
+const galleryMarkup = galleryItems.map(({ preview, original, description }) => {
+   return `<a class="gallery__item" href="${original}">
+  <img class="gallery__image" src="${preview}" alt="${description}" />
+</a>`;
+}).join('');
+
+gallery.insertAdjacentHTML('beforeend', galleryMarkup);
+gallery.addEventListener('click', onClickOpenModal);
+
+function onClickOpenModal(event) {
+    event.preventDefault();
+
+    if (event.target.nodeName !== "IMG") {
+        return;
+    }
+
+    const lightbox = new SimpleLightbox('.gallery a',  {
+        captionsData: "alt",
+        captionDelay: "250",
+        captionPosition: "bottom",
+    });
+    
+}
